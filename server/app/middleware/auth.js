36d1775f688e8f0e86d1d28587c2526523bcdd1a
Bartosz Niziołek <34310@student.anstar.edu.pl>
2023-12-08 21:04:41 +0100

    add role check in auth middleware

--- server/app/middleware/auth.js
+++ server/app/middleware/auth.js
@@ -1,20 +1,28 @@
-import jwt from 'jsonwebtoken';
-import config from '../config';
+import jwt from "jsonwebtoken";
+import config from "../config";
 
-const auth = (req, res, next) => {
-  let token = req.headers['x-access-token'] || req.headers['authorization'];
-  if (token.startsWith('Bearer ')) {
-    token = token.slice(7, token.length);
-  }
-  if (!token) return res.status(401).send('Access denied. No token provided.');
+const roleAuth = (allowedRoles) => {
+  return (req, res, next) => {
+    let token = req.headers["x-access-token"] || req.headers["authorization"];
+    if (token && token.startsWith("Bearer ")) {
+      token = token.slice(7, token.length);
+    }
+    if (!token)
+      return res.status(401).send("Access denied. No token provided.");
 
-  try {
-    req.user = jwt.verify(token, config.JwtSecret);
-    next();
-  }
-  catch (ex) {
-    res.status(400).send('Invalid token.');
-  }
+    try {
+      const decodedToken = jwt.verify(token, config.JwtSecret);
+      req.user = decodedToken;
+
+      if (allowedRoles.includes(decodedToken.role)) {
+        next();
+      } else {
+        res.status(403).send("Access denied. Insufficient role.");
+      }
+    } catch (ex) {
+      res.status(400).send("Invalid token.");
+    }
+  };
 };
 
-export default auth;
+export default roleAuth;
