ca3165e88f4ef971214394ea0704aa89c662e9e3
Bartosz Niziołek <34310@student.anstar.edu.pl>
2023-12-13 12:31:47 +0100

    add page with all events

--- ticketify/src/app/components/login/login.component.ts
+++ ticketify/src/app/components/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -9,13 +10,14 @@ import { AuthService } from '../../services/auth.service';
 export class LoginComponent {
   loginUserData = { email: '', password: '' };
 
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private router: Router) {}
 
   loginUser(): void {
     this.authService.loginUser(this.loginUserData).subscribe(
       (res) => {
         console.log('Zalogowano pomyślnie');
         localStorage.setItem('token', res.token);
+        this.router.navigate(['/events']);
       },
       (err) => console.error('Błąd logowania', err)
     );
